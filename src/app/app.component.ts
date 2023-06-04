706729d2e8e0f2591e4ee2c8aec290f6169509d7
Almusty1993 <alanamumusty@gmail.com>
2023-06-04 15:24:30 -0400

    Please help me see what is wrong

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OLPProject';
+
+	active = 1;
+
 }
